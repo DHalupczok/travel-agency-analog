6c8a5ea864d3aafa0aa109d2ef06d7bbca835298
Denis Halupczok <denis.halupczok.external@atos.net>
2024-01-26 13:18:58 +0100

    some changes made

--- src/app/pages/(default-layout)/home.page.ts
+++ src/app/pages/(default-layout)/home.page.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-
+import {Component, Signal} from '@angular/core';
+import {toSignal} from "@angular/core/rxjs-interop";
+import {injectLoad} from "@analogjs/router";
+import {load} from './home.server'
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   template: `
-     <p>home page works!!</p>
+     <p>home page works!! {{data().loaded}}</p>
   `,
 })
 export default class HomePage {
+  data: Signal<{loaded: boolean}> = toSignal(injectLoad<typeof load>(), { requireSync: true });
 }
